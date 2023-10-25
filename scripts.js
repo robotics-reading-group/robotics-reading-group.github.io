32c7a0dde6987ca488db731b25e0d98ee3494402
robotics-reading-group <149004712+robotics-reading-group@users.noreply.github.com>
2023-10-25 22:06:55 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('papers.csv')
+    // Replace with your Google Sheet CSV link
+    const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1pj_bHTd7RDTGN4NDmmmD8KTYK-4Nvla5rMgvPJZPpoY/export?format=csv';
+
+    fetch(GOOGLE_SHEET_CSV_URL)
     .then(response => response.text())
     .then(data => {
         const rows = data.split('\n').slice(1); // skip the header row
         let htmlContent = '';
 
         rows.forEach(row => {
-            const [presenter, title, linkp, year, source,linkm] = row.split(',');
+            const [presenter, title, linkp, year, source, linkm] = row.split(',');
+
+            // Ensure that the row has the expected number of columns to avoid errors
+            if ([presenter, title, linkp, year, source, linkm].includes(undefined)) {
+                return;
+            }
+
             htmlContent += `
                 <tr>
-                    <td>${presenter}</td>
-                    <td>${title}</td>
-                    <td><a href="${linkp}" target="_blank">Link paper</a></td>
-                    <td>${year}</td>
-                    <td>${source}</td>
-                    <td><a href="${linkm}" target="_blank">Link meeting</a></td>
+                    <td>${presenter.trim()}</td>
+                    <td>${title.trim()}</td>
+                    <td><a href="${linkp.trim()}" target="_blank">Link paper</a></td>
+                    <td>${year.trim()}</td>
+                    <td>${source.trim()}</td>
+                    <td><a href="${linkm.trim()}" target="_blank">Link meeting</a></td>
                 </tr>
             `;
         });
 
-        document.querySelector('#papersTable tbody').innerHTML = htmlContent;
+        document.querySelector('#papersTable tbody').innerHTML = htmlContent; // add the rows to the table
     })
     .catch(error => {
         console.error('Error fetching the CSV file:', error);
