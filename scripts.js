68f86e9ffef7ba600af9e2ba8abac51f0a070390
robotics-reading-group <149004712+robotics-reading-group@users.noreply.github.com>
2023-10-26 08:14:30 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Replace with your Google Sheet CSV link
-    const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1pj_bHTd7RDTGN4NDmmmD8KTYK-4Nvla5rMgvPJZPpoY/export?format=csv';
+    const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1mzFGnrAVxXgl-67nVLEZ2WCv2U8sHbeMnyPhpwSMZzs/export?format=csv';
 
     fetch(GOOGLE_SHEET_CSV_URL)
     .then(response => response.text())
@@ -10,20 +10,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
         rows.forEach(row => {
             // Splitting by tab character since CSV provided is tab-separated
-            const [date, agenda, presenter, source, authors] = row.split('\t');
+            const [date, agenda, presenter, source] = row.split('\t');
 
             // Ensure that the row has the expected number of columns to avoid errors
-            if ([date, agenda, presenter, source, authors].includes(undefined)) {
+            if ([date, agenda, presenter, source].includes(undefined)) {
                 return;
             }
 
             htmlContent += `
                 <tr>
                     <td>${date.trim()}</td>
-                    <td>${agenda.trim()}</td>
+                    <td><a href="${source.trim()}" target="_blank">${agenda.trim()}</a></td>
                     <td>${presenter.trim()}</td>
                     <td>${source.trim()}</td>
-                    <td>${authors.trim()}</td>
                 </tr>
             `;
         });
